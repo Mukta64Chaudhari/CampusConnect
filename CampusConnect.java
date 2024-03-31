8956da1e5ee809155c4b8b5530a82b0d02418429
aryadavare19 <learnarya2005@gmail.com>
2024-03-31 22:58:14 +0530

    t

--- CampusConnect.java
+++ CampusConnect.java
@@ -1,7 +1,7 @@
 public class CampusConnect {
     public static void main(String[] args) {
         System.out.println("welcome to campus connect");
-        System.out.println("hi mukta here");
+        System.out.println("hi arya here");
         System.out.println("we are working ");
     }
 }
